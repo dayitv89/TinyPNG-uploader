b3176b770fcb2305600ab0eb46361f43d08f6464
Vikram Biwal <crazyvk56@gmail.com>
2017-07-05 21:31:11 +0530

    code added

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-var config = require('config');
+var fs = require('fs');
+var tinify = require("tinify");
+var path = require('path')
 
-var tinify = require('tinify');
+if (process.argv.length <= 2) {
+    console.log("Usage: please insert tinipng key as first argument");
+    process.exit(-1);
+}
 
-// TODO: read recursive folder name as terminal argument
-// TODO: Find all images with supported file image extension (use config.IMAGE_EXTENSIONS)
-// TODO: Validate image size(use config.IMAGE_SIZE), log error IF exceed.
-// TODO: upload files using queue (use this variable to config.QUEUE_SIZE)
-// TODO: download all files to respective file path.
-// TODO: log error IF any occurs.
+if (process.argv.length <= 3) {
+    console.log("Usage: " + __filename + " path/to/directory");
+    process.exit(-1);
+}
+
+tinify.key = process.argv[2];
+var path = process.argv[3];
+
+fs.readdir(path, function(err, items) {
+    for (var i=0; i<items.length; i++) {
+        console.log(path + '/' +  items[i]);
+        if(getFileExtensions(items[i], ['png', 'PNG'])) {
+          var fileuri = path + '/' +  items[i]
+          tinify.fromFile(fileuri).toFile(fileuri);
+        }
+    }
+});
+
+function getFileExtensions(filename, extensions) {
+  var ext = (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename)[0] : undefined;
+  return ext ? extensions.indexOf(ext) > -1 : false;
+}
